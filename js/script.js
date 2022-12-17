6412a338f85442a30120cd5bdcebab316ee494c1
Olga <olg200919@ya.ru>
2022-12-17 11:00:20 +0300

    Задача 3

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
 /* 1. При изменении значения в input с id="from", значение содержащееся в нем должно моментально отображаться в span. То есть при печати в input'е тег span также должен меняться.
-*/
+
 document.querySelector('#from').addEventListener('input', event => {
     document.querySelector('span').innerHTML = event.target.value;
 });
+*/
 
 /* 2. При клике на кнопку с классом messageBtn необходимо элементу с классом message:
 1) добавить два класса: animate_animated и animate_fadeInLeftBig
 2) поставить данному элементу стиль visibility в значение 'visible'.
-*/
+
 document.querySelector('.messageBtn').addEventListener('click', () => {
     document.querySelector('.message').classList.add('animate__animated', 'animate__fadeInLeftBig');
     document.querySelector('.message').style.visibility = 'visible';
 });
+*/
 
 /* 3. Необходимо при отправке формы проверить, заполнены ли все поля в этой форме. Если какое-либо поле не заполнено, форма не должна отправляться, также должны быть подсвечены незаполненные поля (необходимо поставить класс error незаполненным полям).
 Как только пользователь начинает заполнять какое-либо поле, необходимо, при вводе в данное поле, произвести проверку:
 Если поле пустое, необходимо данное поле подсветить (поставить класс error данному полю).
 Если поле было чем-либо заполнено, подсветку (класс error)необходимо убрать.
 */
+const formEl = document.querySelector('form');
+const fieldsInp = document.querySelectorAll('.form-control');
+
+formEl.addEventListener('submit', () => {
+  fieldsInp.forEach(field => {
+    if (field.value === '') {
+      event.preventDefault();
+      field.classList.add('error');
+    };
+  });
+});
+
+formEl.addEventListener('input', () => {
+  fieldsInp.forEach(field => {
+    if (field.value === '') {
+      field.classList.add('error');
+    } else if (field.value !== '') {
+      field.classList.remove('error');
+    };
+  });
+});
